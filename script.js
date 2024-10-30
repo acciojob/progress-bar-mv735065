a3556952410668094bef1ba1611a241aa68817b2
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-10-30 18:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,57 +1,134 @@
-//your JS code here. If required.
-const circles=document.querySelectorAll('.circle')
-const lines=document.querySelectorAll('.line')
-const prevbutton=document.querySelector('#prev')
-const nextbutton=document.querySelector('#next')
+// //your JS code here. If required.
 
+let circle=document.querySelectorAll(".circle");
+let line=document.querySelectorAll(".line");
 
-  let curr=1
-nextbutton.addEventListener('click',()=>{
-    curr++
-        if(curr>=circles.length){
-            curr=circles.length
-        }
+let next=document.querySelector("#next");
+let prev=document.querySelector("#prev");
 
-        ui_update()
-  console.log(curr)
+let value=1;
 
-})
+next.addEventListener("click",()=>{
+	value++;
+	
+	if(value>circle.length) value=circle.length;
 
-prevbutton.addEventListener('click', (event) => {
-    curr--;
-    if (curr <= 1) {
-        curr = 1;
-    }
-   ui_update()
+	
+	
+	if(value==circle.length){
+		next.disabled=true;
+	}
+	if(value!=1) prev.disabled=false;
 
-    console.log(curr);
+	ui_update();
 });
 
-function ui_update(){
-     circles.forEach((circle,index) => {
-        if(index<curr){
-            circle.classList.add('active');
-        }
-        else {
-          circle.classList.remove('active');
-        }
-        
-    });
+prev.addEventListener("click",()=>{
+	value--;
+	if(value<=0) value=1;
 
-    lines.forEach((line,index) => {
-        if(index<curr-1){
-            line.classList.add('active');
-        }
-        else {
-        
-             line.classList.remove('active');
-        }
-        
-    });
+	
+	
+	if(value==1){
+		prev.disabled=true;
+	}
+	else next.disabled=false;
 
-if(curr>1){
-    prevbutton.disabled=false;
+	ui_update();
+});
+
+ 
+function ui_update() {
+
+	for(let i=1;i<=circle.length;i++){
+		if(i<=value)
+		circle[i-1].classList.add("active");
+		else circle[i-1].classList.remove("active");
+	}
+	
+		
+	// }
+	for(let i=1;i<=line.length;i++){
+		if(i<=value-1)
+		line[i-1].classList.add("active");
+		else line[i-1].classList.remove("active");
+	}
+	
+	
 }
 
-}
+// function ui_update() {
+
+// 	for(let i=1;i<=value;i++){
+// 		circle[i-1].classList.add("active");
+// 	}
+// 	for(let i=1;i<value;i++){
+// 		line[i-1].classList.add("active");
+// 	}
+	
+// }
+
+
+
+
+
+
+
+
+
+// const circles=document.querySelectorAll('.circle')
+// const lines=document.querySelectorAll('.line')
+// const prevbutton=document.querySelector('#prev')
+// const nextbutton=document.querySelector('#next')
+
+
+//   let curr=1
+// nextbutton.addEventListener('click',()=>{
+//     curr++
+//         if(curr>=circles.length){
+//             curr=circles.length
+//         }
+
+//         ui_update()
+//   console.log(curr)
+
+// })
+
+// prevbutton.addEventListener('click', (event) => {
+//     curr--;
+//     if (curr <= 1) {
+//         curr = 1;
+//     }
+//    ui_update()
+
+//     console.log(curr);
+// });
+
+// function ui_update(){
+//      circles.forEach((circle,index) => {
+//         if(index<curr){
+//             circle.classList.add('active');
+//         }
+//         else {
+//           circle.classList.remove('active');
+//         }
+        
+//     });
+
+//     lines.forEach((line,index) => {
+//         if(index<curr-1){
+//             line.classList.add('active');
+//         }
+//         else {
+        
+//              line.classList.remove('active');
+//         }
+        
+//     });
+
+// if(curr>1){
+//     prevbutton.disabled=false;
+// }
+
+// }
 
